f3f5310ad2ea53ef8d172c13a1f372c0389d27eb
kazashim <kazashimkuzasuwat@gmail.com>
2020-06-29 20:46:02 +0200

    first

---
+++ lic/zampay.php
@@ -0,0 +1,20 @@
+<?php 
+
+class zampay
+
+{
+    protected $user;
+    protected $password;
+    protected $msisd;
+    protected $shortcode;
+    protected $trasactionid;
+    protected $amount;
+}
+
+public function __construct($password, $msisd, $shortcode )
+
+{
+    
+}
+
+?>
