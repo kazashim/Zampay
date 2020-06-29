8d7bffa41f819ba8e8f4b81bba5fd0f72e08f9c9
kazashim <kazashimkuzasuwat@gmail.com>
2020-06-29 22:45:30 +0200

    update

--- zampay.php
+++ zampay.php
@@ -9,12 +9,18 @@ class zampay
     protected $Shortcode;
     protected $Trasactionid;
     protected $Amount;
+    protected $base_url;
 
-    
-    public function __construct($ThirdPartyID, $password, $msisd, $shortcode )
+
+    public function __construct($ThirdPartyID, $Password, $Msisdn, $Shortcode )
 
     {
         
+        $this->ThirdPartyID = $ThirdPartyID;
+        $this->Password = $Password;
+        $this->Msisdn =  $Msisdn;
+        $this->Shortcode = $Shortcode;
+
     }
     
 
