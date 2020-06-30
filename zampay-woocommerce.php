6d5018a82ce62a4ec41bd9dc90c7898240d54b7f
kazashim <kazashimkuzasuwat@gmail.com>
2020-06-30 22:51:18 +0200

    UPDATE

--- zampay-woocommerce.php
+++ zampay-woocommerce.php
@@ -8,8 +8,20 @@
  * License: GNU GPLv3
  */
 
-define( 'MOMOPAY_PLUGIN_DIR_PATH', plugin_dir_path( __FILE__ ) );
+define( 'ZAMPAY_PLUGIN_DIR_PATH', plugin_dir_path( __FILE__ ) );
 
-require_once( MOMOPAY_PLUGIN_DIR_PATH . 'Zampay-sdk/lib/Momopay.php' );
-require_once( MOMOPAY_PLUGIN_DIR_PATH . 'mtn-momopay-php-sdk/lib/EventHandlerInterface.php' );
-require_once( MOMOPAY_PLUGIN_DIR_PATH . 'mtn-momopay-php-sdk/lib/MomopayEventHandler.php' );
+require_once( ZAMPAY_PLUGIN_DIR_PATH . 'zampay-sdk/lib/Zampaypay.php' );
+require_once( ZAMPAY_PLUGIN_DIR_PATH . 'zampay-sdk/lib/EventHandlerInterface.php' );
+
+use Zamtel\MomopayEventHandler;
+use Zamtel\Zampay;
+
+/**
+ * This action hook registers our PHP class as a WooCommerce payment gateway
+ */
+add_filter( 'woocommerce_payment_gateways', 'Zampaypay_add_gateway_class' );
+
+function zampaypay_add_gateway_class($gateways ) {
+    $gateways[] = 'WC_Zampay_Gateway';
+    return $gateways;
+}
