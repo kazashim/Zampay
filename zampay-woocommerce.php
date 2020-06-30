0c786d053e12c8fa3f9e55d0a753fc9962ac54db
kazashim kuzasuwat <kazashimkuzasuwat@gmail.com>
2020-06-30 12:41:56 +0200

    Update zampay-woocommerce.php

---
+++ zampay-woocommerce.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Plugin Name: Zampay WooCommerce Payment Gateway
+ * Description: WooCommerce payment gateway for Zampay
+ * Author: kazashim kuzasuwat
+ * Author URI: http://
+ * Version: 1.0.0
+ * License: GNU GPLv3
+ */
